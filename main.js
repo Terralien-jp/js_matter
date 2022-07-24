1d09c7a1362f6e680fc27a3d5fd11adf91d46cc2
Terralien <terralienjp@gmail.com>
2022-07-24 12:38:15 +0900

    fix main.js

--- main.js
+++ main.js
@@ -26,12 +26,52 @@ let rect = Bodies.rectangle(400, 0, 100, 80);
 // 円オブジェクト
 let cir = Bodies.circle(200, 0, 50);
 let cir2 = Bodies.circle(600, 0, 50);
+let cir3 = Bodies.circle(700, 0, 60, {
+    restitution: 1,
+    render: {
+        fileStyle: '#AAAAAA',
+        strokeStyle: '#000000',
+        lineWidht: 5
+    }
+});
 
 World.add(world, [
     rect,
     cir,
-    cir2
+    cir2,
+    cir3
 ]);
+
+let cir4 = Bodies.circle(80, 0, 50, {
+    restitution: 1,
+    render: {
+        sprite:{
+            texture: 'sample.png'
+        }
+    }
+});
+
+let MouseConstraint = Matter.MouseConstraint,
+    Mouse = Matter.Mouse;
+
+// マウスコントロール
+var mouse = Mouse.create(render.canvas),
+    mouseConstraint = MouseConstraint.create(engine, {
+        mouse: mouse,
+        constraint: {
+            stiffness: 0.1,
+        }
+    });
+World.add(world, mouseConstraint);
+
+render.mouse = mouse;
+
+// フロアの作成
+let floor = Bodies.rectangle(400, 400, 800, 40, {
+    inStatic: true
+});
+
+World.add(world, floor);
 
 Render.run(render);
 Engine.run(engine);
